7494aa133c835fe065e024238306b77913139297
SijimolCyriac <50610989+SijimolCyriac@users.noreply.github.com>
2021-07-03 21:06:07 +0530

    Update DbConne.php

--- DbConne.php
+++ DbConne.php
@@ -1,3 +1,4 @@
 <?php
-  $con=mysqli_connect("localhost","root","","constdb") or die("COULDN'T CONNECT");
+//$con=mysqli_connect("localhost","root","","constdb") or die("COULDN'T CONNECT");
+$con=mysqli_connect("remotemysql.com","7PEUbikaIM","2qpLIRLEC8","7PEUbikaIM") or die("COULDN'T CONNECT");
 ?>
